f3f66475b869637b5b62e520a3c6b4be2d078059
Daniel Ferragut <danielpferragut@gmail.com>
2019-10-05 17:44:47 -0300

    Basic BAT operation done (except check)

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include "bats.h"
 #include "queue.h"
 
-pthread_mutex_t mutex_array[5];
+pthread_mutex_t mutex;
 pthread_cond_t cond_array[5];
 pthread_t dir_array[4];
 Queue* priority_queue[4];
@@ -32,20 +32,71 @@ void* queue_start(void* arg){
     pthread_exit(NULL);
 }
 
-void* BAT_manager(char* dir_string){
-    printf("[BATMAN]Starting BATMAN with - %s", dir_string);
+void BAT_manager_init(){
     Directions dir =  NORTH;
     pthread_create(&dir_array[0], NULL, queue_start, &dir);
-    pthread_cond_wait(&cond_array[0], &mutex_array[0]);
+    pthread_cond_wait(&cond_array[0], &mutex);
     dir = EAST;
     pthread_create(&dir_array[1], NULL, queue_start, &dir);
-    pthread_cond_wait(&cond_array[0], &mutex_array[0]);
+    pthread_cond_wait(&cond_array[0], &mutex);
     dir = SOUTH;
     pthread_create(&dir_array[2], NULL, queue_start, &dir);
-    pthread_cond_wait(&cond_array[0], &mutex_array[0]);
+    pthread_cond_wait(&cond_array[0], &mutex);
     dir = WEST;
     pthread_create(&dir_array[3], NULL, queue_start, &dir);
-    pthread_cond_wait(&cond_array[0], &mutex_array[0]);
+    pthread_cond_wait(&cond_array[0], &mutex);
+}
+
+
+void arrive(Directions dir){
+    BAT* current_car = new_car(total_car_number, dir);
+    total_car_number++;
+    printf("BAT %d %c chegou no cruzamento\n", current_car->car_number, enum_to_chr(current_car->dir));
+    switch (current_car->dir){
+        case NORTH:
+            push(priority_queue[0], current_car);
+            break;
+        case EAST:
+            push(priority_queue[1], current_car);
+            break;
+        case SOUTH:
+            push(priority_queue[2], current_car);
+            break;
+        case WEST:
+            push(priority_queue[3], current_car);
+            break;
+    }
+}
+
+void car_exit(BAT* current_car){
+    printf("BAT %d from %c saiu do cruzamento\n", current_car->car_number, enum_to_chr(current_car->dir));
+    switch (current_car->dir){
+        case NORTH:
+            pop(priority_queue[0]);
+            break;
+        case EAST:
+            pop(priority_queue[1]);
+            break;
+        case SOUTH:
+            pop(priority_queue[2]);
+            break;
+        case WEST:
+            pop(priority_queue[3]);
+            break;
+    }
+}
+
+void cross(BAT* current_car){
+    pthread_mutex_lock(&mutex);
+    sleep(1);
+    car_exit(current_car);
+    pthread_mutex_unlock(&mutex);
+}
+
+
+void* BAT_manager(char* dir_string){
+    printf("[BATMAN]Starting BATMAN with - %s", dir_string);
+    BAT_manager_init();
 
     printf("[BATMAN] Threads created, now carty cars!\n");
     char current_dir = dir_string[0];
@@ -64,14 +115,10 @@ void* BAT_manager(char* dir_string){
 }
 
 void initialize_thread_array(){
-    pthread_mutex_t main_mutex, north_queue_mutex, east_queue_mutex, south_queue_mutex, west_queue_mutex;
+    pthread_mutex_t main_mutex;
     pthread_cond_t main, north_queue, east_queue, south_queue, west_queue;
 
     pthread_mutex_init(&main_mutex, NULL);
-    pthread_mutex_init(&north_queue_mutex, NULL);
-    pthread_mutex_init(&east_queue_mutex, NULL);
-    pthread_mutex_init(&south_queue_mutex, NULL);
-    pthread_mutex_init(&west_queue_mutex, NULL);
     pthread_cond_init(&main, NULL);
     pthread_cond_init(&north_queue, NULL);
     pthread_cond_init(&east_queue, NULL);
@@ -83,11 +130,7 @@ void initialize_thread_array(){
     cond_array[2] = east_queue;
     cond_array[3] = south_queue;
     cond_array[4] = west_queue;
-    mutex_array[0] = main_mutex;
-    mutex_array[1] = north_queue_mutex;
-    mutex_array[2] = east_queue_mutex;
-    mutex_array[3] = south_queue_mutex;
-    mutex_array[4] = west_queue_mutex;
+    mutex = main_mutex;
 
     Queue* north = create_queue();
     Queue* east = create_queue();
